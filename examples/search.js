2dff209b283ef70508a132df1266321155bb875a
Yosuke Furukawa <furukawa.yosuke@dena.jp>
2014-03-19 11:48:02 +0900

    Fix JSON parse

--- examples/search.js
+++ examples/search.js
@@ -1,6 +1,8 @@
 var tiqav = require("../index");
 var callback = function(err, result) {
   console.log(result);
+  console.log(result[0].id);
+  console.log(tiqav.createImageUrl(result[0].id, result[0].ext));
 };
 
 tiqav.search.search("jojo", callback);
--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 var Search = require("./lib/search");
+module.exports.createImageUrl = function(id, ext) {
+    return "http://img.tiqav.com/" + id + "." + ext;
+}
+module.exports.createThumbnailUrl = function(id) {
+    return "http://img.tiqav.com/" + id + ".th.jpg";
+}
 
 module.exports.search = new Search();
